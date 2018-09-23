4bc5bb02fe8e7824e33dbe49d5139d28a8138414
Gökhan Çıplak <gciplak90@gmail.com>
2018-09-23 21:29:57 +0300

    Random, randomX, and randomMLE for system evaluator

--- src/main/java/edu/anadolu/eval/SystemEvaluator.java
+++ src/main/java/edu/anadolu/eval/SystemEvaluator.java
@@ -11,12 +11,20 @@ import edu.anadolu.knn.Prediction;
 import edu.anadolu.knn.Solution;
 import org.apache.commons.collections4.map.HashedMap;
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.StatUtils;
 import org.clueweb09.InfoNeed;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class SystemEvaluator{
 
+    private Metric metric;
+    private int k;
     private final Map<Tag,Evaluator> tagEvaluatorMap;
     private Set<String> modelIntersection;
     private List<String> tags;
@@ -32,6 +40,8 @@ public class SystemEvaluator{
 
     public SystemEvaluator(List<String> tags,DataSet dataSet,Measure measure, String evalDirectory,String op) {
         this.tags=tags;
+        this.metric=measure.metric();
+        this.k=measure.k();
         tagEvaluatorMap = new HashMap<>();
         for (String tag: tags) {
             final Evaluator evaluator = new Evaluator(dataSet, tag, measure, "all", evalDirectory, op);
@@ -44,6 +54,8 @@ public class SystemEvaluator{
         this.tagEvaluatorMap = tagEvaluatorMap;
         tags = new ArrayList<>();
         tagEvaluatorMap.keySet().stream().forEach(tag -> tags.add(tag.name()));
+        this.metric=tagEvaluatorMap.entrySet().stream().findAny().get().getValue().metric;
+        this.k=tagEvaluatorMap.entrySet().stream().findAny().get().getValue().k;
         initialize();
     }
 
@@ -178,38 +190,60 @@ public class SystemEvaluator{
         }
     }
     public void printOracleMax(){
-        oracleMaxAsSolution().stream().forEach(solution -> System.out.println(solution.key+" max:"+solution.getMean()));
-    }
-    public List<Solution> oracleMaxAsSolution() {
-        List<Solution> solutionList = new ArrayList<>(modelIntersection.size());
-
-        for (String model : modelIntersection) {
-            List<Prediction> list = new ArrayList<>(needs.size());
-
-            for (InfoNeed testQuery : needs) {
-                double bestScore = Double.NEGATIVE_INFINITY;
-                String bestTag = null;
-
-                for (String tag : tags) {
-                    double score = tagEvaluatorMap.get(Tag.tag(tag)).score(testQuery, model);
-                    if (score > bestScore) {
-                        bestScore = score;
-                        bestTag = tag;
-                    }
-                }
-
-                if (null == bestTag) throw new RuntimeException("predictedModel is null!");
-
-                Prediction prediction = new Prediction(testQuery, bestTag, bestScore);
-                list.add(prediction);
-            }
-            Solution solution = new Solution(list, -1);
-            calculateAccuracy(solution,model);
-
-            solution.setKey("Oracle\t" + model);
-            solutionList.add(solution);
+        for(String model:modelIntersection) {
+            Solution s = oracleMaxAsSolution(needs, model);
+            System.out.println(s.key + " max:\t" + s.getMean()+"\t"+s.hits0+"\t"+s.hits1+"\t"+s.hits2);
         }
-        return solutionList;
+    }
+    public void printRandom(){
+        for(String model:modelIntersection) {
+            Solution s = randomAsSolution(needs, model);
+            System.out.println(s.key + ":\t" + s.getMean()+"\t"+s.hits0+"\t"+s.hits1+"\t"+s.hits2);
+        }
+    }
+
+    public void printRandomMLE(){
+        for(String model:modelIntersection) {
+            Solution s = randomMLE(needs, model);
+            System.out.println(s.key + " :\t " + s.getMean()+"\t"+s.hits0+"\t"+s.hits1+"\t"+s.hits2);
+        }
+    }
+
+    public void printRandomX(){
+        for(String model:modelIntersection) {
+            SystemScore s = randomX(model);
+            System.out.println(s.system + " : " + s.score);
+        }
+    }
+
+    public Solution oracleMaxAsSolution(List<InfoNeed> needs, String model) {
+
+        List<Prediction> list = new ArrayList<>(needs.size());
+
+        for (InfoNeed testQuery : needs) {
+            double bestScore = Double.NEGATIVE_INFINITY;
+            String bestTag = null;
+
+            for (String tag : tags) {
+                double score = tagEvaluatorMap.get(Tag.tag(tag)).score(testQuery, model);
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestTag = tag;
+                }
+            }
+
+            if (null == bestTag) throw new RuntimeException("predictedSystem is null!");
+
+            Prediction prediction = new Prediction(testQuery, bestTag, bestScore);
+            list.add(prediction);
+        }
+        Solution solution = new Solution(list, -1);
+        calculateAccuracy(solution, model);
+
+        solution.setKey("Oracle\t" + model);
+        solution.model = model;
+        return solution;
+
     }
 
     public void printFacets() {
@@ -222,6 +256,28 @@ public class SystemEvaluator{
         }
     }
 
+    public void printHighestScoresWithCoV(boolean residualNeeds){
+        for(String model:modelIntersection) {
+            Solution s;
+            if(residualNeeds) s = oracleMaxAsSolution(residualNeeds(model),model);
+            else s = oracleMaxAsSolution(needs,model);
+            System.out.println("-----" + model + "-----");
+            System.out.printf("Query\tSystem\t%s_%d\tCOV\n", metric, k);
+            for(Prediction p: s.list){
+                double variance = varianceMap.get(p.testQuery).get(s.model);
+                System.out.printf("%d\t%s\t%.4f\t%.4f\n", p.testQuery.id(), p.predictedModel, p.predictedScore, variance);
+            }
+            System.out.printf("Average:\t%.4f\n", s.getMean());
+        }
+    }
+
+    public List<InfoNeed> residualNeeds(String model) {
+
+        List<InfoNeed> residualNeeds = new ArrayList<>(needs);
+        residualNeeds.removeAll(modelAllSameMap.get(model));
+        residualNeeds.removeAll(modelAllZeroMap.get(model));
+        return residualNeeds;
+    }
     private Multiset<String> facet(String model) {
 
         Multiset<String> multiset = HashMultiset.create();
@@ -264,7 +320,7 @@ public class SystemEvaluator{
         for (InfoNeed need : needs) {
             if (modelAllZeroMap.get(model).contains(need) || modelAllSameMap.get(model).contains(need))
                 continue;
-            LinkedHashSet<String> winners = multiLabelWinners(need, se,model);
+            LinkedHashSet<String> winners = multiLabelWinners(need, se, model);
             multiLabelMap.put(need, winners);
         }
         return multiLabelMap;
@@ -412,5 +468,159 @@ public class SystemEvaluator{
         }
 
         return sum;
+    }
+
+    public Solution randomAsSolution(String model) {
+        return randomAsSolution(this.needs, model);
+    }
+    public Solution randomAsSolution(List<InfoNeed> needs,String model) {
+
+        List<Prediction> list = new ArrayList<>(needs.size());
+
+        List<String> systemSet = new ArrayList<>(tags);
+
+
+        boolean isUnix;
+        Random r=null;
+        DataInputStream is=null;
+        try {
+            is = new DataInputStream(Files.newInputStream(Paths.get("/dev/urandom")));
+            isUnix=true;
+        } catch (IOException ioe) {
+            //System.err.println("Error while getting random integers from /dev/urandom. Random number is being generated vie Random class");
+            isUnix=false;
+            r = new Random();
+        }
+
+        for (InfoNeed testQuery : needs) {
+            int randomIndex;
+            if(isUnix) {
+                try{ randomIndex = Math.abs(is.readInt()) % systemSet.size();
+                }catch (IOException io){throw new RuntimeException("is.readInt() random integers from /dev/urandom", io);}
+            }
+            else randomIndex = Math.abs(r.nextInt()) % systemSet.size();
+            String predictedSystem = systemSet.get(randomIndex);
+            double predictedScore = tagEvaluatorMap.get(Tag.tag(predictedSystem)).score(testQuery, model);
+
+            Prediction prediction = new Prediction(testQuery, predictedSystem, predictedScore);
+            list.add(prediction);
+        }
+
+        Solution solution = new Solution(list, -1);
+        calculateAccuracy(solution,model);
+
+        solution.setKey("RND\t" + model);
+        solution.model = model;
+
+        return solution;
+    }
+
+    public Solution randomMLE(List<InfoNeed> needs,String model) {
+        return randomMLE(needs, randomMLEMap(model), model);
+    }
+    public Map<String, Double> randomMLEMap(String model) {
+
+        Map<String, Double> map = new HashMap<>();
+
+        int residualNeedSize = needs.size() - modelAllSameMap.get(model).size() - modelAllZeroMap.get(model).size();
+
+        for (String system : tags) {
+
+            // a model may not exist in best model map, e.g. winner for zero queries
+            if (!bestModelSystemMap.get(model).containsKey(system)) {
+                map.put(system, 0.0);
+                continue;
+            }
+
+            int count = bestModelSystemMap.get(model).get(system).size();
+
+            map.put(system, (double) count / residualNeedSize);
+
+        }
+
+        return map;
+
+    }
+
+    public Solution randomMLE(List<InfoNeed> needs, Map<String, Double> map,String model) {
+
+        List<String> systemSet = new ArrayList<>(tags);
+
+        /**
+         * How many times a system winner?
+         * That much addition is done for the system
+         */
+        List<String> localSystems = new ArrayList<>(needs.size());
+
+        for (String system : map.keySet()) {
+
+            int count = Math.round(map.get(system).floatValue() * (float) needs.size());
+            for (int i = 0; i < count; i++)
+                localSystems.add(system);
+        }
+
+        List<Prediction> list = new ArrayList<>(needs.size());
+
+
+        boolean isUnix;
+        Random r=null;
+        DataInputStream is=null;
+        try {
+            is = new DataInputStream(Files.newInputStream(Paths.get("/dev/urandom")));
+            isUnix=true;
+        } catch (IOException ioe) {
+            //System.err.println("Error while getting random integers from /dev/urandom. Random number is being generated vie Random class");
+            isUnix=false;
+            r = new Random();
+        }
+
+        for (int i = localSystems.size(); i < needs.size(); i++) {
+            int randomIndex;
+            if (isUnix) {
+                try {
+                    randomIndex = Math.abs(is.readInt()) % systemSet.size();
+                } catch (IOException io) {
+                    throw new RuntimeException("is.readInt() random integers from /dev/urandom", io);
+                }
+            } else randomIndex = Math.abs(r.nextInt()) % systemSet.size();
+            localSystems.add(systemSet.get(randomIndex));
+        }
+
+
+        Collections.shuffle(localSystems);
+        for (InfoNeed testQuery : needs) {
+
+            String predictedSystem = localSystems.remove(0);
+            double predictedScore = tagEvaluatorMap.get(Tag.tag(predictedSystem)).score(testQuery, model);
+
+
+            Prediction prediction = new Prediction(testQuery, predictedSystem, predictedScore);
+            list.add(prediction);
+        }
+
+
+        Solution solution = new Solution(list, -1);
+        calculateAccuracy(solution,model);
+
+        solution.setKey("MLE\t" + model);
+        solution.model = model;
+
+        return solution;
+
+    }
+
+    public double randomXScore(String model) {
+        return randomMLE(this.needs,model).getMean();
+    }
+
+    public SystemScore randomX(String model) {
+
+        double[] means = new double[500];
+
+        for (int i = 0; i < 500; i++) {
+            means[i] = randomXScore(model);
+        }
+
+        return new SystemScore(String.format("RandomMLE (\u00B1%.2f)", Math.sqrt(StatUtils.variance(means))), StatUtils.mean(means));
     }
 }
